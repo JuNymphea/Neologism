6f8d1d3400b1069082873de0f5617909e0768020
JuNymphea <zj153@georgetown.edu>
2025-11-09 09:50:28 +0800

    train & eval code with chat_template

---
+++ scripts/train_neologism.sh
@@ -0,0 +1,11 @@
+source ~/.bashrc
+conda activate disrpt
+
+python neologism/scripts/train_neologism.py --concept short
+
+# python neologism/scripts/eval_neologism.py --concept short --concept_tokenizer_path neologism/checkpoints/tokenizer --concept_model_path neologism/checkpoints/checkpoint-263
+# python neologism/scripts/eval_neologism.py --concept short --concept_tokenizer_path neologism/checkpoints/tokenizer --concept_model_path neologism/checkpoints/checkpoint-526
+# python neologism/scripts/eval_neologism.py --concept short --concept_tokenizer_path neologism/checkpoints/tokenizer --concept_model_path neologism/checkpoints/checkpoint-789
+
+
+# python neologism/scripts/eval_neologism.py --concept short --concept_tokenizer_path neologism/model/google/gemma-3-4b-it --concept_model_path neologism/model/google/gemma-3-4b-it
